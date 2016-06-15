7d176e87fc1c6ebed79a3594e8a6a0f76a257268
shenmj053 <shenmj053@163.com>
2016-06-15 21:04:57 +0800

    Front page HTML now generate from a templage

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,7 @@ class NewVistorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
+
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -30,7 +31,8 @@ class NewVistorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         self.fail('Finish the test!')    
